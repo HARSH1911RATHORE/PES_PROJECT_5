c55b4c2d6628ffb127628c4da28a84e8b7207699
HARSH1911RATHORE <55412102+HARSH1911RATHORE@users.noreply.github.com>
2019-11-20 15:53:02 -0700

    Add files via upload

--- UART.c
+++ UART.c
@@ -2,8 +2,30 @@
 
 #include <stdio.h>
 
+
+
 //Q_T TxQ, RxQ;
 Circilar_Buffer TxQ, RxQ;
+
+
+
+/* BEGIN - UART0 Device Driver
+
+
+
+	Code created by Shannon Strutz
+
+	Date : 5/7/2014
+
+	Licensed under CC BY-NC-SA 3.0
+
+	http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+
+
+	Modified by Alex Dean 9/13/2016
+
+*/
 
 FILE __stdout;  //Use with printf
 
@@ -15,7 +37,13 @@ struct __FILE
 
 };
 
+
+
 FILE __stdin;		//use with fget/sscanf, or scanf
+
+
+
+
 
 //Retarget the fputc method to use the UART0
 
@@ -29,6 +57,8 @@ int fputc(int ch, FILE *f){
 
 }
 
+
+
 //Retarget the fgetc method to use the UART0
 
 int fgetc(FILE *f){
@@ -39,6 +69,8 @@ int fgetc(FILE *f){
 
 }
 
+
+
 // Code listing 8.8, p. 231
 
 void Init_UART0(uint32_t baud_rate) {
@@ -46,6 +78,8 @@ void Init_UART0(uint32_t baud_rate) {
 	uint16_t sbr;
 
 	uint8_t temp;
+
+
 
 	// Enable clock gating for UART0 and Port A
 
@@ -167,24 +201,49 @@ void Init_UART0(uint32_t baud_rate) {
 
 	UART0->S1 &= ~UART0_S1_RDRF_MASK;
 
+
+
 }
 
 
+
+/* END - UART0 Device Driver
+
+	Code created by Shannon Strutz
+
+	Date : 5/7/2014
+
+	Licensed under CC BY-NC-SA 3.0
+
+	http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+
+
+	Modified by Alex Dean 9/13/2016
+
+*/
+
+
+
+// Code listing 8.9, p. 233
+
 void UART0_Transmit_Poll(uint8_t data) {
 
-		while (!(UART0->S1 & UART0_S1_TDRE_MASK))               //check for transmit register ready and trasmit
+		while (!(UART0->S1 & UART0_S1_TDRE_MASK))
 
 			;
 
 		UART0->D = data;
-
+//		char c;
+//		c=getchar();
+//		putchar(c);
 }
 
 
 
 uint8_t UART0_Receive_Poll(void) {
 
-		while (!(UART0->S1 & UART0_S1_RDRF_MASK))               //check for receive buffer full and receive
+		while (!(UART0->S1 & UART0_S1_RDRF_MASK))
 
 			;
 
@@ -194,11 +253,12 @@ uint8_t UART0_Receive_Poll(void) {
 
 
 
-// UART0 IRQ Handler
+// UART0 IRQ Handler. Listing 8.12 on p. 235
 
 void UART0_IRQHandler(void) {
 
 	uint8_t ch;
+
 
 
 	if (UART0->S1 & (UART_S1_OR_MASK |UART_S1_NF_MASK |
--- UART.h
+++ UART.h
@@ -4,35 +4,36 @@
 
 
 
-#include <circular_buff.h>
 #include <stdint.h>
 
 #include <MKL25Z4.H>
 
+#include "queue.h"
 
 
 
 #define USE_UART_INTERRUPTS 	(0) // 0 for polled UART communications, 1 for interrupt-driven
 
-#define UART_OVERSAMPLE_RATE 	(16)			//oversampling ratio
+#define UART_OVERSAMPLE_RATE 	(16)
 
-#define BUS_CLOCK 			(24e6)				//bus clock value
+#define BUS_CLOCK 						(24e6)
 
-#define SYS_CLOCK			(48e6)          //systick clock value
+#define SYS_CLOCK							(48e6)
 
 
 
 void Init_UART0(uint32_t baud_rate);
 
-void UART0_Transmit_Poll(uint8_t data);                 //transmit poll and receive poll
+void UART0_Transmit_Poll(uint8_t data);
 
 uint8_t UART0_Receive_Poll(void);
 
 
 
 void Send_String_Poll(uint8_t * str);
-                                               //send characters one by one till array not equal to null
+
 void Send_String(uint8_t * str);
+
 
 
 uint32_t Rx_Chars_Available(void);
@@ -40,7 +41,9 @@ uint32_t Rx_Chars_Available(void);
 uint8_t	Get_Rx_Char(void);
 
 
-extern Circilar_Buffer TxQ, RxQ;               //extern to access in different files Q_T TxQ, RxQ
+
+//extern Q_T TxQ, RxQ;
+extern Circilar_Buffer TxQ, RxQ;
 
 
 
